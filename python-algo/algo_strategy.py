86f21cde80cd4d58b88a11d41efbb265c58f8341
jmficker <jmficker@uwaterloo.ca>
2021-03-12 00:00:43 -0500

    added demolisher strat

--- python-algo/algo_strategy.py
+++ python-algo/algo_strategy.py
@@ -43,7 +43,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         SP = 0
         # This is a good place to do initial setup
         self.invert = False # whether we are inverted or not
-        self.dem_attack = False  # whether to prepare for a demolisher + wall attack
+        self.dem_attack_stage = 0  # whether to prepare for a demolisher + wall attack (0 - don't prep, 1 - prep, 2 - attack)
         self.scored_on_locations = []
 
 
@@ -96,37 +96,165 @@ class AlgoStrategy(gamelib.AlgoCore):
         """
         Defense:
         """
-        self.get_normalized_point([3,4])
+        mp_available = game_state.get_resource(MP)
+        enemy_mp = game_state.get_resource(MP, 1)
+        self.get_normalized_point([3,4])  # wat is dis for
 
-        self.build_defenses_v1(game_state)
+        right_side_walls = self.get_normalized_points([[14, 2], [15, 3], [16, 4], [17, 5], [18, 6], [19, 7], [20, 8], [21, 9],
+                                                      [23, 11], [22, 12], [24, 12], [25, 12], [26, 12]])
+        bottom_closure = right_side_walls + self.get_normalized_points([[13, 3], [12, 3], [11, 3], [10, 3]])
+        left_side_walls = self.get_normalized_points([[9, 4], [8, 5], [7, 6], [6, 7], [5, 8], [4, 9], [3, 10], [2, 11]])
+        full_v = bottom_closure + left_side_walls
+
+        # check if they have a vulnerable line in the front
+        enemy_front_vul_y = self.enemy_front_vulnerable(game_state)
+        gamelib.debug_write("Front enemy y loc attack: {}".format(enemy_front_vul_y))
+        if enemy_front_vul_y != -1 and mp_available >= 12 and self.total_structures(game_state) > 25:
+            self.dem_attack_stage = 1
+
+        deploy_int = False
+        if enemy_mp >= 9 and self.dem_attack_stage == 3:
+            deploy_int = True
+            self.dem_attack_stage = 0
+
+        if self.dem_attack_stage == 0:
+            self.build_defenses_v1(game_state, deploy_int)
+        elif self.dem_attack_stage == 1:
+            self.build_defenses_v1(game_state, deploy_int)
+            self.demolisher_stage1_v1(game_state)
+        elif self.dem_attack_stage == 2:
+            self.demolisher_stage2_v1(game_state, enemy_front_vul_y)
+
         if game_state.turn_number < 4:
-            game_state.attempt_spawn(INTERCEPTOR, [[2, 11], [20, 6]], 2)  # interceptor stalling while base is built
-        else:
-            mp_available = game_state.get_resource(MP)
-            #game_state.attempt_spawn(INTERCEPTOR, [[23, 9], [16, 2]], 1)
-            if (mp_available > 12 and game_state.turn_number < 30) or (mp_available > 16):
+            game_state.attempt_spawn(INTERCEPTOR, self.get_normalized_points([[6, 7], [20, 6]]), 2)  # interceptor stalling while base is built
+        #elif self.enemy_front_vulnerable(game_state) and mp_available >= 12 and self.structures_intact_v1(game_state, weak_dem_walls_needed):
+            #gamelib.debug_write("Weak demolisher attack.")
+            #game_state.attempt_spawn(DEMOLISHER, self.get_normalized_point([15, 1]), 10000)
+        elif enemy_mp >= 9 and self.dem_attack_stage < 2:  # send interceptor for defense while rebuilding
+            if not self.structures_intact_v1(game_state, full_v):
+                if enemy_mp < 12:
+                    game_state.attempt_spawn(INTERCEPTOR, [20, 6], 1)
+                    game_state.attempt_spawn(INTERCEPTOR, [6, 7], 1)
+                else:
+                    game_state.attempt_spawn(INTERCEPTOR, [20, 6], 2)
+                    game_state.attempt_spawn(INTERCEPTOR, [6, 7], 2)
+            else:
+                if enemy_mp < 12:
+                    game_state.attempt_spawn(INTERCEPTOR, [21, 7], 2)
+                    game_state.attempt_spawn(INTERCEPTOR, [15, 1], 1)
+                else:
+                    game_state.attempt_spawn(INTERCEPTOR, [20, 6], 2)
+                    game_state.attempt_spawn(INTERCEPTOR, [6, 7], 2)
+        elif enemy_mp >= 6 and self.dem_attack_stage < 2 and not game_state.contains_stationary_unit([6, 7]):
+            game_state.attempt_spawn(INTERCEPTOR, [6, 7], 1)
+        if ((mp_available >= 12 and game_state.turn_number < 30) or (mp_available >= 22 and game_state.turn_number > 50)
+            or (mp_available > 16 and 50 >= game_state.turn_number >= 30)) and self.dem_attack_stage == 0:  # scout attack (not during demolisher attack prep)
+            if game_state.turn_number > 70 and random.randint(1, 4) == 3:
+                game_state.attempt_spawn(DEMOLISHER, [13, 0], 100000)
+            elif random.randint(1,4) == 4:
                 game_state.attempt_spawn(SCOUT, [13, 0], math.floor(mp_available * 0.25))
                 game_state.attempt_spawn(SCOUT, [11, 2], math.floor(mp_available * 0.75))
 
-    def build_defenses_v1(self, game_state):
+    def total_structures(self, game_state):
+        total_num = 0
+        for location in game_state.game_map:
+            if location[1] <= 13:
+                if game_state.contains_stationary_unit(location):
+                    total_num += 1
+        return total_num
+
+    def structures_intact_v1(self, game_state, locations):
+        for location in locations:
+            if not game_state.contains_stationary_unit(location):  # if the loc does not contain a building
+                return False  # then we do not have our whole structure intact -> missing piece
+        return True
+
+    def demolisher_stage1_v1(self, game_state):
+        gamelib.debug_write("Demolisher stage 1.")
+        for location in game_state.game_map:
+            if location[1] <= 13:
+                if game_state.contains_stationary_unit(location):
+                    game_state.attempt_remove(location)
+
+        self.dem_attack_stage += 1
+        game_state.attempt_spawn(INTERCEPTOR, [14, 0], 1)
+        game_state.attempt_spawn(INTERCEPTOR, [21, 7], 1)
+
+    def demolisher_stage2_v1(self, game_state, enemy_y):
+        gamelib.debug_write("Demolisher stage 2.")
+
+        if game_state.get_resource(SP) > 20:  # we need enough SP to build a semi-respectable wall
+            for x in range(27, 9, -1):  # spawn as many walls as we can (and remove)
+                game_state.attempt_spawn(WALL, self.get_normalized_point([x, enemy_y - 2]))
+                game_state.attempt_remove(self.get_normalized_point([x, enemy_y - 2]))
+            for x in range(21, 5, -1):
+                game_state.attempt_spawn(SUPPORT, self.get_normalized_point([x, enemy_y - 4]))
+                game_state.attempt_remove(self.get_normalized_point([x, enemy_y - 4]))
+            for x in range(21, 5, -1):
+                game_state.attempt_upgrade(self.get_normalized_point([x, enemy_y - 4]))
+            game_state.attempt_spawn(DEMOLISHER, self.get_normalized_point([24, 10]), 10000)
+        else:
+            gamelib.debug_write("Aborting demolisher stage 2: Insufficient SP")
+        self.dem_attack_stage += 1
+
+    def enemy_front_vulnerable(self, game_state):
+        """
+        :return: True if the enemy is vulnerable to a demolisher + wall attack
+        """
+        num_turrets = 0
+        num_upgr_turr = 0
+        num_struct = 0
+        most_turrets_y = -1
+        most_struct_y = -1
+        for y in range(14, 16):
+            for x in range(0, 27):
+                unit = game_state.contains_stationary_unit(self.get_normalized_point([x, y]))
+                if unit:
+                    if unit.unit_type in [WALL, TURRET]:
+                        if unit.unit_type == TURRET:
+                            num_turrets += 1
+                            if unit.upgraded:
+                                num_upgr_turr += 1
+                    num_struct += 1
+            if num_turrets > 7:
+                if num_upgr_turr <= 2:  # basically the upgraded turrets demolish the demolishers if they are stacked
+                    most_turrets_y = y
+            num_turrets = 0
+            if num_struct > 22:
+                most_struct_y = y
+            num_struct = 0
+
+        if most_turrets_y != -1:
+            return most_turrets_y
+        elif most_struct_y != -1:
+            return most_struct_y
+        else:
+            return -1
+
+
+    def build_defenses_v1(self, game_state, deploy_int):
         # TODO: replace turrets + walls every turn
 
         supp_pts = [[14, 3], [13, 2], [19, 8], [20, 9], [19, 9], [18, 7], [17, 6], [16, 5], [15, 4]]
         supp_stages = [0, 0, 2, len(supp_pts)]
         supp_upgr_stages = [2, 2, 2, len(supp_pts)]
 
-        turret_pts = [[3, 13], [24, 13], [1, 12], [2, 12], [2, 13], [21, 10], [25, 13], [3, 12], [20, 11], [22, 13],
-                      [4, 13], [3, 11], [23, 12], [19, 10], [19, 11], [20, 10], [4, 12], [24, 12]]
-        turret_stages = [2, 3, 5, len(turret_pts)]
+        turret_pts = [[3, 13], [24, 13], [1, 12], [24, 12], [2, 12], [21, 10], [25, 13], [3, 12], [20, 11], [22, 13],
+                      [4, 13], [3, 11], [23, 12], [23, 13], [19, 10], [19, 11], [20, 10], [4, 12]]
+        turret_stages = [2, 4, 5, len(turret_pts)]
         turret_upgr_stages = turret_stages
 
-        wall_pts = [[13, 3], [14, 2], [26, 12], [27, 13], [0, 13], [1, 13], [15, 3], [16, 4], [17, 5], [18, 6],
-                       [12, 3], [11, 3], [10, 3], [9, 4], [8, 5], [7, 6], [6, 7], [5, 8], [4, 9], [3, 10], [2, 11],
+        wall_pts = [[13, 3], [14, 2], [26, 12], [27, 13], [0, 13], [1, 13], [2, 13], [15, 3], [16, 4], [17, 5], [18, 6],
+                       [12, 3], [11, 3], [10, 3], [9, 4], [8, 5], [7, 6], [5, 8], [4, 9], [3, 10], [2, 11],
                     [19, 7], [20, 8], [21, 9], [22, 12], [23, 11], [26, 13], [25, 12], [19, 12], [21, 13]]
-        wall_upgr_pts = [[0, 13], [1, 13], [27, 13], [26, 13], [26, 12], [1, 12], [25, 12], [21, 13], [22, 12],
+
+        if not deploy_int:
+            wall_pts.append([6, 7])
+
+        wall_upgr_pts = [[0, 13], [1, 13], [2, 13], [27, 13], [26, 13], [26, 12], [1, 12], [25, 12], [21, 13], [22, 12],
                          [19, 12]]  # order for upgrading walls
         wall_stages = [2, 24, len(wall_pts), len(wall_pts)]
-        wall_upgr_stages = [0, 4, 4, len(wall_upgr_pts)]
+        wall_upgr_stages = [4, 4, 4, len(wall_upgr_pts)]
 
         if game_state.turn_number == 1:
             game_state.attempt_spawn(TURRET, turret_pts[:2])
